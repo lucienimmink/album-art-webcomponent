b7b5810ba31f4c4f5917061c19353f938397f62a
Lucien Immink <lucien.immink@isaac.local>
2019-07-16 16:48:30 +0200

    Fix virtual scroll,  updating the node takes time, which messes with the effect, since we do not REMOVE elements, we only UPDATE them; by using the html literal.

--- src/virtual-scroll.js
+++ src/virtual-scroll.js
@@ -9,8 +9,7 @@ class VirtualScroll extends LitElement {
       screenItemsLength: { type: Number, reflect: true },
       maxBuffer: { type: Number, reflect: true },
       cachedItemLength: { type: Number, reflect: true },
-      pre: { type: Number, reflect: true },
-      post: { type: Number, reflect: true },
+      total: { type: Number, reflect: true },
     };
   }
   static get styles() {
@@ -29,56 +28,50 @@ class VirtualScroll extends LitElement {
     this.height = this.itemHeight * 2; // 440
     this.screenItemsLength = Math.ceil(this.height / this.itemHeight); // 2
     this.maxBuffer = this.screenItemsLength * this.itemHeight; // 440
-    this.cachedItemLength = this.screenItemsLength * 3; // keep a cache // 6
+    this.cachedItemLength = this.screenItemsLength * 4; // keep a cache // 6
   }
   updated(changedProperties) {
     changedProperties.forEach((oldValue, propName) => {
       if (propName === "list") {
+        this.total = this.list.length * this.itemHeight;
         this.renderChunk(0);
       }
     });
   }
   render() {
     return html`
-      <div @scroll="${this.handleScroll}" style="height: ${this.height}px; overflow: auto;">
-        <div style="height: ${this.pre}px;"></div>
+      <div @scroll="${this.handleScroll}" style="height: ${this.height}px; overflow: auto; position: relative; padding: 0;">
         ${this.chunk}
-        <div style="height: ${this.post}px;"></div>
+        <div style="height: ${this.post}px; opacity: 0; position: absolute; top 0; left: 0; width: 1px; height: ${this.total}px "></div>
       </div>
     `;
   }
   handleScroll(e) {
     const scrollTop = e.target.scrollTop; // Triggers reflow
-    //if (!this.lastPaintY || Math.abs(scrollTop - this.lastPaintY) > this.maxBuffer) {
-    // recalc
-    // const first = parseInt(scrollTop / this.itemHeight) - this.screenItemsLength;
-    // console.log(parseInt(scrollTop / this.itemHeight), this.screenItemsLength);
-    const first = parseInt(scrollTop / this.itemHeight);
-    this.renderChunk(first < 0 ? 0 : first);
-    //  this.lastPaintY = scrollTop;
-    //}
+    if (!this.lastPaintY || Math.abs(scrollTop - this.lastPaintY) > this.maxBuffer) {
+      // recalc
+      const first = parseInt(scrollTop / this.itemHeight) - this.screenItemsLength;
+      this.renderChunk(first < 0 ? 0 : first);
+      this.lastPaintY = scrollTop;
+    }
   }
   renderChunk(first) {
     let last = first + this.cachedItemLength;
     if (last > this.list.length) {
       last = this.list.length;
     }
-    console.log(`render chunck starting with ${first} and ending with ${last}`);
     const list = this.list.slice(); // copy
     const chunk = list.splice(first, last - first);
-    this.pre = first * this.itemHeight;
-    this.post = (this.list.length - 1 - last) * this.itemHeight;
-    // this.requestUpdate();
 
     this.chunk = html`
       ${chunk.map(
-        artist =>
+        (artist, index) =>
           html`
-            <album-art artist="${artist}"></album-art>
+            <album-art artist="${artist}" style="position: absolute; top: ${(first + index) * this.itemHeight}px"></album-art>
           `
       )}
     `;
-    //this.requestUpdate();
+    this.requestUpdate();
   }
 }
 
