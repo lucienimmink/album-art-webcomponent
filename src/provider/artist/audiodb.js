757a758fb1df6a9d96fac01b731a5cf340bde8b8
Lucien Immink <lucien@arielext.org>
2019-07-29 16:40:05 +0200

    Use either fanrt or artistthumb

--- src/provider/artist/audiodb.js
+++ src/provider/artist/audiodb.js
@@ -1,10 +1,14 @@
 const fetchArt = async artist => {
-  const response = await fetch(`https://www.theaudiodb.com/api/v1/json/1/search.php?s=${encodeURIComponent(artist)}`);
+  const response = await fetch(
+    `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${encodeURIComponent(
+      artist
+    )}`
+  );
   if (response.status === 200) {
     const json = await response.json();
     const { artists } = json;
     if (artists) {
-      return artists[0].strArtistFanart;
+      return artists[0].strArtistFanart || artists[0].strArtistThumb;
     }
   }
   throw Error("no art found in provider audiodb");
